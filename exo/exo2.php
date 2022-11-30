b4d083e16044f21dcdc21c89e21f18a9cad48d34
Hautemanière <edouard.hte@gmail.com>
2022-11-30 16:50:38 +0100

    exo 2 fini

--- exo/exo2.php
+++ exo/exo2.php
@@ -1,22 +1,65 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset='utf-8'>
-        <title>Exo 2 php</title>
-    </head>
-    <body>
-        <?php
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Exo 2 php</title>
+</head>
+<body>
+   
+            <?php
 
-        $tableau = array();
-        $tableau[0] => '2'
-        $tableau[1] => '2'
-        $tableau[2] => '2'
-        $tableau[3] => '2'
-        $tableau[4] => '2'
-        $tableau[5] => '2'
-        $tableau[6] => '2'
+         /*---------------------------------------------------------------------------------------------------
+
+		Exercice 2.1
+		
+            A) Créer un tableau php qui contient 5 chiffres.
+            Afficher les 5 chiffres à la suite grace au parcours du tableau dans une boucle for.
+
+            B) Essayer d’afficher ses 5 chiffres dans un tableau HTML que génère PHP
+
+         ---------------------------------------------------------------------------------------------------*/
 
 
-        ?>
-    </body>
+         $a=array(
+            0 => 1,
+            1 => 13,
+            2 => 26,
+            3 => 52,
+            4 => 106,
+            5 => 212,
+          );
+            for($i=0; $i< 5; $i++)
+            {
+               echo '['.$a[$i]."]";
+            }
+            ?>
+
+            <?php
+            
+          /*---------------------------------------------------------------------------------------------------
+
+		
+		Exercice 2.2
+			
+            A) Créer un tableau php qui contient un nom un prenom et un mot de passe et qui a pour indice de colonne: Nom, Prenom, MDP.
+
+            Afficher ce tableau avec la commande echo Tableau[‘Nom’] ect
+
+         ---------------------------------------------------------------------------------------------------*/
+
+            $tabStr = array(
+
+               "Nom" => "HAUTEMANIERE",
+               "Prénom" => "Edouard",
+               "MDP" => "eXoaKalAs"
+
+            );
+
+            echo " <br> Nom = ".$tabStr["Nom"]. ", Prénom = ".$tabStr["Prénom"]. ", MDP = ".$tabStr["MDP"];
+
+            ?>
+   
+</body>
 </html>
