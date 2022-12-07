b0c00f0e000ab91fc48bd121dc1df627876ef8bc
Hautemaniere <113522498+Hautemaniere@users.noreply.github.com>
2022-12-07 15:17:55 +0100

    fin exo 4 + debut exo5

---
+++ exo/exo5.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <title>Exo 5 php</title>
+    </head>
+    <body>
+            <?php
+
+         /*---------------------------------------------------------------------------------------------------
+
+   		   Exercice 5
+
+		    Créer un petit formulaire avec une zone de text et un bouton. Lorsque vous cliquer sur le bouton la 
+        
+            page doit afficher ce que vous avez saisie en rouge. Utilisez $_POST[‘champ1’]
+
+         ---------------------------------------------------------------------------------------------------*/
+
+
+
+
+            ?>
+
+
+    </body>
+</html>
