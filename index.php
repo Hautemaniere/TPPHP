0b0086d242215d82d1975e9e0e3c0e04e5add9e3
Hautemaniere <113522498+Hautemaniere@users.noreply.github.com>
2022-12-07 16:12:21 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -3,12 +3,17 @@
     <head>
         <meta charset='utf-8'>
         <title>Acceuil</title>
+
     </head>
     <body>
 
     <a href="exo/exo1.php">Exercice 1</a>
     <a href="exo/exo2.php">Exercice 2</a>
     <a href="exo/exo3.php">Exercice 3</a>
+    <a href="exo/exo4.php">Exercice 4</a>
+    <a href="exo/exo5.php">Exercice 5</a>
+    <a href="exo/exo6.php">Exercice 6</a>
+    <a href="exo/exo7.php">Exercice 7</a>
 
     </body>
 </html>
